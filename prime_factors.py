9f860e4037edc4e912fbba46159657becbdb8665
seungwoo <lirtha@naver.com>
2024-06-13 10:09:10 +0900

    [refactoring] 7/7 : 코드 리팩토링

--- prime_factors.py
+++ prime_factors.py
@@ -3,14 +3,11 @@ class PrimeFactor:
         factors = []
 
         if number > 1:
-            if number in [4, 6, 9, 12]:
-                divisor = 2
-                while number > 1:
-                    while number % divisor == 0:
-                        number = number // divisor
-                        factors.append(divisor)
-                    divisor += 1
-            else:
-                factors.append(number)
+            divisor = 2
+            while number > 1:
+                while number % divisor == 0:
+                    number = number // divisor
+                    factors.append(divisor)
+                divisor += 1
 
         return factors
