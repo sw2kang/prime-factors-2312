3d5e4045a200bbd9fc9a5cc5cc9a74ebd7eb9463
seungwoo <lirtha@naver.com>
2024-06-13 09:41:57 +0900

    [feature] 2/7 : '2' 테스트 통과 코드 작성

--- prime_factors.py
+++ prime_factors.py
@@ -1,3 +1,6 @@
 class PrimeFactor:
     def of(self, param):
+        if param == 2:
+            return [2]
+
         return []
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -7,3 +7,7 @@ class TestPrimeFactor(TestCase):
     def test_prime_factor_1(self):
         prime_factor = PrimeFactor()
         self.assertEqual([], prime_factor.of(1))
+
+    def test_prime_factor_2(self):
+        prime_factor = PrimeFactor()
+        self.assertEqual([2], prime_factor.of(2))
