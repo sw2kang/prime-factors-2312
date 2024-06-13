6565532ce4f9c10359843c09a0bf8001f6e0205b
seungwoo <lirtha@naver.com>
2024-06-13 09:09:56 +0900

    add test

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 
+from prime_factors import PrimeFactor
+
 
 class TestPrimeFactor(TestCase):
-    pass
+    def test_prime_factor_1(self):
+        prime_factor = PrimeFactor()
+        self.assertEqual(1, 1)
