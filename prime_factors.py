44917e13487e90aa679afd4b6c3327b1f4765d08
seungwoo <lirtha@naver.com>
2024-06-13 09:49:44 +0900

    [feature] 4/7 : '4' 테스트 통과 코드 작성

--- prime_factors.py
+++ prime_factors.py
@@ -2,7 +2,10 @@ class PrimeFactor:
     def of(self, number):
         factors = []
 
-        if number >= 2:
+        if 3 >= number >= 2:
             factors.append(number)
+        elif number >= 4:
+            factors = [2, 2]
+
 
         return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -16,3 +16,6 @@ class TestPrimeFactor(TestCase):
 
     def test_prime_factor_3(self):
         self.assertEqual([3], self.prime_factor.of(3))
+
+    def test_prime_factor_4(self):
+        self.assertEqual([2, 2], self.prime_factor.of(4))
