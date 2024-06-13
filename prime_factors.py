8a8afea03c68aa63d5021c84276f44a9ee4cc6c3
seungwoo <lirtha@naver.com>
2024-06-13 09:46:58 +0900

    [refactoring] 3/7 : 코드 리팩토링

--- prime_factors.py
+++ prime_factors.py
@@ -2,10 +2,7 @@ class PrimeFactor:
     def of(self, number):
         factors = []
 
-        if number == 2:
-            factors.append(2)
-
-        if number == 3:
-            factors.append(3)
+        if number >= 2:
+            factors.append(number)
 
         return factors
